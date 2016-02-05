611cb004119575acf44ce1d226b8a10a61e8b904
AriellaS <ariella.simoni@gmail.com>
2016-02-04 17:34:01 -0800

    Fixes filter contours

--- src/Main.java
+++ src/Main.java
@@ -18,14 +18,28 @@ public class Main {
 	public static void main(String[] args) {
 		
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-		
+		/*
+		Mat frame = Imgcodecs.imread("images/colorcancel.png");
+		Mat colors = new Mat();
+		//cancelColors(frame, frame);
+		Imgproc.cvtColor(frame, colors, Imgproc.COLOR_BGR2GRAY);
+		List<MatOfPoint> contours = findContours(colors);
+		System.out.println(contours.size());
+		drawContours(frame, contours);
+		Imgcodecs.imwrite("contours.png", frame);
+		List<MatOfPoint2f> points2f = approxPoly(contours);
+		for(int i = 0; i < points2f.get(0).toList().size(); i++) {
+			System.out.println(points2f.get(0).toList().get(i));
+		}
+		*/
 		VideoCapture camera = new VideoCapture();
 		camera.open(1);
 		Mat frame = new Mat();
 		camera.read(frame);
 		findGoal(camera);
+		
 		/*
-//		Mat original = new Mat();
+		Mat original = new Mat();
 		Mat converted = new Mat();
 		Mat colorsCanceled = new Mat();
 		
@@ -65,8 +79,8 @@ public class Main {
 		Imgproc.erode(output, output, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(5, 5)));
 		Imgproc.dilate(output, output, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(5, 5)));
 		
-		Imgproc.dilate(output, output, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(50, 50)));
-		Imgproc.erode(output, output, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(50, 50)));
+		//Imgproc.dilate(output, output, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(50, 50)));
+		//Imgproc.erode(output, output, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(50, 50)));
 		
 		Imgproc.blur(output, output, new Size(5,5));
 	}
@@ -100,15 +114,16 @@ public class Main {
 	}
 	
 	public static List<MatOfPoint> filterContours(List<MatOfPoint> contours) {
+		List<MatOfPoint> newContours = new ArrayList<MatOfPoint>();
 		for(int i = 0; i < contours.size(); i++) {
 			Rect rect = Imgproc.boundingRect(contours.get(i));
 			if(rect.width > 90 && rect.width < 200 
 					&& rect.height > 30 && rect.height < 100
-					&& rect.y < 200) {
-				contours.remove(i);
+					&& rect.y < 250) {
+				newContours.add(contours.get(i));
 			}
 		}
-		return contours;
+		return newContours;
 	}
 	
 	public static List<MatOfPoint2f> approxPoly(List<MatOfPoint> contours) {
@@ -117,13 +132,13 @@ public class Main {
 			MatOfPoint2f temp = new MatOfPoint2f();
 			MatOfPoint2f tempOut = new MatOfPoint2f();
 			temp.fromList(contours.get(i).toList());
-			Imgproc.approxPolyDP(temp, tempOut, 50.0, true);
+			Imgproc.approxPolyDP(temp, tempOut, 10.0, true);
 			if(tempOut.toList().size() > 3 && tempOut.toList().size() < 10) {
 				points.add(tempOut);
 			}
 		}
 		return points;
-	}
+	} 
 	
 	public static List<Rect> findRects(List<MatOfPoint> contours) {
 		List<Rect> rects = new ArrayList<Rect>();
@@ -139,7 +154,7 @@ public class Main {
 	}
 	
 	public static void cancelColors(Mat input, Mat output) {
-		Core.inRange(input, new Scalar(40, 0, 130), new Scalar(115, 255, 255), output);
+		Core.inRange(input, new Scalar(25, 0, 220), new Scalar(130, 80, 255), output);
 	}
 	
 	public static List<Mat> makeSubmats(Mat input, List<Rect> rects) {
@@ -167,10 +182,14 @@ public class Main {
 	
 	public static double findDistance(double vertFOV, double tapeHeight, double camAngle) {
 		double realHeight = 14; // inches
+		double realWidth = 20;
 		double imageHeight = 480; // pixels
 		//double distance = (realHeight * ((imageHeight/2)/(tapeHeight))) / Math.tan(Math.toRadians(vertFOV / 2.0));
 		double distance = (realHeight * ((((vertFOV/2 + camAngle)/vertFOV) * imageHeight)/tapeHeight)) / Math.tan(Math.toRadians(vertFOV/2 + camAngle));
 		//double distance = (realHeight * ((imageHeight/2)/(tapeHeight))) / Math.sin(Math.toRadians(vertFOV/2) * Math.sin(Math.toRadians(90 - vertFOV/2)));
+		
+		//double distance2 = realWidth *   
+		
 		return distance;
 	}
 	
@@ -190,13 +209,29 @@ public class Main {
 			Imgcodecs.imwrite("colors.png", output);
 			
 			List<MatOfPoint> contours = findContours(output);
+
 			List<Rect> rects = findRects(contours);
-			System.out.println(rects.size());
+			//System.out.println(rects.size());
+			
+			contours = filterContours(contours);			
+			List<MatOfPoint2f> points2f = approxPoly(contours);
+			drawContours(frame, contours);
+			Imgcodecs.imwrite("contours.png", frame);
+			
+			System.out.println("points2f size " + points2f.size());
+			for(int i = 0; i < points2f.size(); i++) {
+				System.out.println(points2f.get(i).toList().get(i));
+				System.out.println("amount of points " + points2f.get(i).toList().size());
+				for(int x = 0; x < points2f.get(i).toList().size(); x++) {
+					System.out.println(points2f.get(i).toList().get(i));
+				}
+				System.out.println("new point");
+			}			
 			
 			for(int i = 0; i < rects.size(); i++) {
 				Imgcodecs.imwrite("submat" + i + ".png", makeSubmats(frame, rects).get(i));
-				System.out.println("x: " + rects.get(i).x + " y: " + rects.get(i).y + " width: " + rects.get(i).width + " height: " + rects.get(i).height);
-				System.out.println(findDistance(41.1, findRealHeight(rects.get(i).width), 28));
+				//System.out.println("x: " + rects.get(i).x + " y: " + rects.get(i).y + " width: " + rects.get(i).width + " height: " + rects.get(i).height);
+				//System.out.println(findDistance(44.44, findRealHeight(rects.get(i).width), 20));
 			}
 		}
 	}
